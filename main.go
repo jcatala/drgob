5360e49fa5bacce8c6cb9496354ecdf316e2f6be
pepe <josew.asdf@gmail.com>
2021-03-07 17:25:38 -0300

    deletting bot token, re-generating

--- main.go
+++ main.go
@@ -5,21 +5,35 @@ import (
 	"github.com/joho/godotenv"
 	"log"
 	"fmt"
+	"os"
+	"flag"
 )
 
 func main() {
 	const version = "0.0.1"
+
+	// The items inside flags are just pointers
+	// First, parse all the args that are passed to the bot
+	verbose := flag.Bool("verbose", false, "To be verbose")
+	flag.Parse()
 
 	err := godotenv.Load()
 	if err != nil{
 		log.Fatalln("Error loading the environment variable " )
 	}
 
+	discordkey := ""
+	if *verbose{
+		discordkey = os.Getenv("discord_key")
+		fmt.Printf("Discord key: %s\n", discordkey)
+	}
 
-	discord, err := discordgo.New("Bot " + "ODE4MjEwNDg5NTMxMzY3NDM0.YEUv5g.ni1DKEjdvMhDX4QwWDeS_Sr_OSI")
+
+	discord, err := discordgo.New("Bot " + "a")
 	if err != nil{
 		log.Fatal("Error")
 	}
+
 	fmt.Printf("%p", &discord)
 	
 }
